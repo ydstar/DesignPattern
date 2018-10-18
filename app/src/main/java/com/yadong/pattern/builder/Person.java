5f8c8da543ab2decb43c2dcee567b2e91330d208
houyadong <hydznsqk@163.com>
2018-10-18 10:58:26 +0800

    1.整理

--- app/src/main/java/com/yadong/pattern/builder/Person.java
+++ app/src/main/java/com/yadong/pattern/builder/Person.java
@@ -9,7 +9,7 @@ public class Person {
     private  String name;
     private  int age;
 
-    public Person(Builder builder){
+    private Person(Builder builder){
         name = builder.getName();
         age = builder.getAge();
     }
