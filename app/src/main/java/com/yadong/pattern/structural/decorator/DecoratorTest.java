de5bee855e8d340d7cb1fcaa07dc557af19bbadc
houyadong <hydznsqk@163.com>
2018-11-30 18:21:09 +0800

    1.整理代码

--- app/src/main/java/com/yadong/pattern/structural/decorator/DecoratorTest.java
+++ app/src/main/java/com/yadong/pattern/structural/decorator/DecoratorTest.java
@@ -1,6 +1,5 @@
 package com.yadong.pattern.structural.decorator;
 
-
 import com.yadong.pattern.structural.decorator.v1.BatterCakeWithEgg;
 import com.yadong.pattern.structural.decorator.v1.BatterCakeWithEggSausage;
 import com.yadong.pattern.structural.decorator.v2.ABatterCake;
@@ -9,7 +8,21 @@ import com.yadong.pattern.structural.decorator.v2.EggDecorator;
 import com.yadong.pattern.structural.decorator.v2.SausageDecorator;
 
 /**
+ * 装饰器模式
+ * 定义:在不改变原有对象的基础之上,将功能附加到对象上
+ * 特征:提供了比继承更有弹性的提单方案(扩展原有对象功能)
+ * 类型:结构型
  *
+ * 适用场景:
+ *      1.扩展一个类的功能或给一个类添加附加职责
+ *      2.动态的给一个对象添加功能,这些功能可以再动态的撤销
+ * 优点:
+ *      1.继承的有力补充,比继承灵活,不改变原有对象的情况下给一个对象扩展功能
+ *      2.通过使用不同装饰类以及这些装饰类的排列组合,可以实现不同效果
+ *      3.符合开闭原则
+ * 缺点:
+ *      1.会出现更多的代码,更多的类,增加程序复杂性
+ *      2.动态装饰时,多层装饰会更复杂
  */
 public class DecoratorTest {
 
